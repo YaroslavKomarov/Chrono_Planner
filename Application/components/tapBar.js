71aad7584f7c8b1243e35853c4f5dd9410517321
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-12-21 19:25:02 +0500

    Styles extracted into styles/

--- Application/components/tapBar.js
+++ Application/components/tapBar.js
@@ -1,24 +1,15 @@
 import { Component } from 'react';
 import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
 
+import { tapbarStyles } from '../styles/TapbarStyles';
+
 
 export class Tapbar extends Component {
     render(){
-        return <View style={styles.tapbar}>
+        return <View style={tapbarStyles.tapbar}>
             <TouchableHighlight><Text>Кнопка</Text></TouchableHighlight>
             <TouchableHighlight><Text>Кнопка</Text></TouchableHighlight>
             <TouchableHighlight><Text>Кнопка</Text></TouchableHighlight>
         </View>;
     }
 }
-
-
-const styles = StyleSheet.create({
-    tapbar:{
-        backgroundColor: '#FFEFDF',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
-    }
-});
---
+++ Application/styles/TapbarStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native-web";
+
+
+export const tapbarStyles = StyleSheet.create({
+    tapbar:{
+        backgroundColor: '#FFEFDF',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    }
+});
