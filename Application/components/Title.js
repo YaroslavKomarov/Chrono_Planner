51cfaafcfbc1cae3dd86e726b2da48e68c95bd54
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-12-21 19:22:21 +0500

    Added icon

--- Application/components/Title.js
+++ Application/components/Title.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import { TouchableHighlight, StyleSheet, Text, View, Button } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
 import { titleStyles } from '../styles/TitleStyles';
 
 
@@ -7,7 +9,9 @@ export class Head extends Component {
     render() {
         return <View style={titleStyles.header}>
                 <Text style={titleStyles.caption}>{this.props.title}</Text>
-                <TouchableHighlight style={titleStyles.button} color="#F7C1BD"><Text>Button</Text></TouchableHighlight>
-            </View>; /* вместо текста вставить иконку */
+                <TouchableHighlight style={titleStyles.button} color="#F7C1BD">
+                    <AntDesign name="pluscircleo" size={24} color="black" />
+                </TouchableHighlight>
+            </View>;
     }
 }
