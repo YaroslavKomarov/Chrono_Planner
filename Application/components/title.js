60b5420e5cec6d5e4614d3438281c267aabf3257
Eldar Badalbeyli <inclusion_star@mail.ru>
2021-12-21 19:12:25 +0500

    Styles extracted to styles/

--- Application/components/title.js
+++ Application/components/title.js
@@ -1,34 +1,13 @@
 import { Component } from 'react';
 import { TouchableHighlight, StyleSheet, Text, View, Button } from 'react-native';
+import { titleStyles } from '../styles/TitleStyles';
 
 
 export class Head extends Component {
     render() {
-        return <View style={styles.header}>
-                <Text style={styles.caption}>{this.props.title}</Text>
-                <TouchableHighlight style={styles.button} color="#F7C1BD"><Text>Button</Text></TouchableHighlight>
+        return <View style={titleStyles.header}>
+                <Text style={titleStyles.caption}>{this.props.title}</Text>
+                <TouchableHighlight style={titleStyles.button} color="#F7C1BD"><Text>Button</Text></TouchableHighlight>
             </View>; /* вместо текста вставить иконку */
     }
 }
-
-
-const styles = StyleSheet.create({
-    header:{
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        backgroundColor: '#F7C1BD'
-    },
-    caption:{
-        fontSize: '25px',
-        fontWeight: 'bold',
-        color: '#2F0000',
-        textAlign: 'left',
-        margin: '5%'
-    },
-    button:{
-        textAlign: 'center',
-        alignSelf: 'center',
-        marginRight: '5%'
-    }
-});
