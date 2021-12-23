52ee919e219836ebe48d80c68ec1fde00f558f82
Yaroslav Komarov <yaraslau.komarov@yandex.ru>
2021-12-23 17:43:59 +0500

    add some fix

--- Application/components/AddTaskForm.js
+++ Application/components/AddTaskForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, TouchableOpacity, TextInput, Text } from 'react-native';
 import { Formik } from 'formik';
+import RNPickerSelect from 'react-native-picker-select';
 
 import { formStyles } from '../styles/FormStyles';
 
@@ -26,16 +27,32 @@ export default function AddTaskForm({ modalVisible, setModalVisible, setTask }) 
             }}>
             {(props) => (
                 <View>
-                    <TextInput style={[formStyles.inputText, formStyles.boxShadow]}
+                    <TextInput 
+                        style={[formStyles.inputText, formStyles.boxShadow]}
                         value={props.values.text} 
                         placeholder='Введите текст задачи...' 
                         onChangeText={props.handleChange('text')}
                         />
-                    <TextInput style={[formStyles.inputText, formStyles.boxShadow]}
+                    <TextInput 
+                        style={[formStyles.inputText, formStyles.boxShadow]}
                         value={props.values.type} 
                         placeholder='Выберите тип активности' 
                         onChangeText={props.handleChange('type')}
                         />
+                    {/* <RNPickerSelect
+                        style={[formStyles.inputText, formStyles.boxShadow]}
+                        value={props.values.type} 
+                        placeholder={{
+                            label: 'Выберите тип активности',
+                            value: null,
+                        }}
+                        onValueChange={props.handleChange('type')}
+                        items={[
+                            { label: 'Football', value: 'football' },
+                            { label: 'Baseball', value: 'baseball' },
+                            { label: 'Hockey', value: 'hockey' },
+                        ]} 
+                    /> */}
                     <View style={formStyles.btnContainer}>
                         <TouchableOpacity onPress={props.handleSubmit} style={[formStyles.addButton, formStyles.boxShadow]}>
                             <Text style={formStyles.buttonText}>Создать</Text>
